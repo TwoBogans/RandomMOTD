7dcb65059bdf830b641bc45dcafd0d657fbf2f18
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-05-13 12:56:02 +0200

    Increase max length

--- src/main/java/com/github/anarchyplugins/randommotd/PingListener.java
+++ src/main/java/com/github/anarchyplugins/randommotd/PingListener.java
@@ -7,7 +7,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -31,8 +30,6 @@ public class PingListener implements Listener {
             String prefix = plugin.getConfig().getString("prefix");
             String suffix = plugin.getConfig().getString("suffix");
 
-            System.out.println(Arrays.toString(split[0].toCharArray()));
-
             String nonFormattedMOTD;
 
             switch (split.length) {
@@ -40,7 +37,7 @@ public class PingListener implements Listener {
                     nonFormattedMOTD = prefix + split[0] + "\n" + suffix;
                     break;
                 case 2:
-                    nonFormattedMOTD = prefix + split[0].substring(0, split[0].length() - 1) + "\n" + suffix + split[1].replace("\\s", "");
+                    nonFormattedMOTD = prefix + split[0].substring(0, split[0].length() - 1) + "\n" + suffix + split[1];
                     break;
                 default:
                     nonFormattedMOTD = prefix + "\n" + suffix;
@@ -52,6 +49,6 @@ public class PingListener implements Listener {
     }
 
     private String[] wrap(String str) {
-        return WordUtils.wrap(str, 30, null, true).split("\n");
+        return WordUtils.wrap(str, 44, null, true).split("\n");
     }
 }
