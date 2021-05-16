3fb5ac95632b26161d8f5153e0b8c873e1968d29
RemainingToast <remainingtoast@gmail.com>
2021-05-17 06:07:11 +1000

    Invisible Character Fix

--- src/main/java/com/github/anarchyplugins/randommotd/RandomMOTD.java
+++ src/main/java/com/github/anarchyplugins/randommotd/RandomMOTD.java
@@ -34,24 +34,17 @@ public final class RandomMOTD extends JavaPlugin {
         String[] split = wrap(getRandomMotd());
 
         String prefix = getConfig().getString("prefix");
+
         String suffix = getConfig().getString("suffix");
 
-        String motd;
-
-        if (split.length == 1) {
-            motd = prefix + split[0] + "\n" + suffix;
-        } else if (split.length == 2) {
-            motd = prefix + split[0].substring(0, split[0].length() - 1) + "\n" + suffix + split[1];
-        } else if (split.length > 2) {
-            motd = prefix + split[0].substring(0, split[0].length() - 1) + "\n" + suffix + split[1].substring(0, split[1].length() - 1);
-        } else {
-            motd = prefix + "\n" + suffix;
-        }
-
-        return ChatColor.translateAlternateColorCodes('&', motd);
+        return ChatColor.translateAlternateColorCodes('&',
+                split.length == 1 ? prefix + split[0] + "\n" + suffix : (
+                split.length >= 2 ? prefix + split[0] + "\n" +
+                suffix + split[1] : prefix + "\n" + suffix)
+        );
     }
 
     private String[] wrap(String str) {
-        return WordUtils.wrap(str, getConfig().getInt("wrap", 40), null, true).split("\n");
+        return WordUtils.wrap(str, getConfig().getInt("wrap", 45), null, true).split("\n");
     }
 }
